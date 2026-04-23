83cae6ea86243758e274f5da69e291e2c01d54a7
joaomiguelfermen-art <joaomiguelfermen@gmail.com>
2026-04-23 19:20:23 +0000

    adicionei printf no código!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,11 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  int n; 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
-
+printf("Ola Mundo, no Codespace\n");
+  printf("Exemplo de edição de código!\n");
 return 0;
 } 
