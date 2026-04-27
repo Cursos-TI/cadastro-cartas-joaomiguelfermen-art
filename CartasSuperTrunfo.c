cfd16b2cfb11d4ca53382cc669eb4022e615aa0d
joaomiguelfermen-art <joaomiguelfermen@gmail.com>
2026-04-27 19:35:42 +0000

    adicionei as informações da cidade do Rio de Janeiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,13 +4,17 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  int n, total; 
+  
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
-printf("Ola Mundo, no Codespace\n");
-  printf("Exemplo de edição de código!\n");
+  char Rio ;de Janeiro[100];
+  int populacao = 17.22 ;milhões;
+  float area = 43.696 Km²;
+  float pib = 1.3 Trilhões de Reais;
+  float idh = 0.6613;
+  float numero de pontos turísticos = 55;
 return 0;
 } 
