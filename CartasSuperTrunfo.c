3349d0f1cf39c82903a8c6e1474c66810b100476
joaomiguelfermen-art <joaomiguelfermen@gmail.com>
2026-05-06 19:29:03 +0000

    adicionei as variáveis de densidade populacional e PIB per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,17 +4,25 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+  int main(void)
+{
+    char RioDeJaneiro[100] = "Rio de Janeiro";
+    int populacao = 17220000;
+    float area = 43.696f;
+    float pib = 1.3f;
+    float idh = 0.6613f;
+    int numero_de_pontos_turisticos = 55;
+    float densidade_populacional = populacao / area;
+    float pib_per_capita = 1020000000000.0f / 17220000.0f;
 
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-  
-  // Área para entrada de dados
+    printf("Cidade: %s\n", RioDeJaneiro);
+    printf("População: %d\n", populacao);
+    printf("Área: %.3f km²\n", area);
+    printf("PIB: %.1f trilhões de reais\n", pib);
+    printf("IDH: %.4f\n", idh);
+    printf("Número de pontos turísticos: %d\n", numero_de_pontos_turisticos);
+    printf("Densidade populacional: %.2f habitantes por km²\n", densidade_populacional);
+    printf("PIB per capita: %.2f reais\n", pib_per_capita);
 
-  // Área para exibição dos dados da cidade
-  char Rio ;de Janeiro[100];
-  int populacao = 17.22 ;milhões;
-  float area = 43.696 Km²;
-  float pib = 1.3 Trilhões de Reais;
-  float idh = 0.6613;
-  float numero de pontos turísticos = 55;
-return 0;
-} 
+    return 0;
+}
